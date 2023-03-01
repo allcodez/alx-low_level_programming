ac3ffca45867dab11f130c8f6f98167ba75caa9e
FahdAdebayo <fahdadebayo02@gmail.com>
2023-03-01 12:21:48 -0800

    C

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * *_strcat - concatenates @src to @dest
  * @src: the source string to append to @dest
@@ -9,14 +10,12 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int dest_len = strlen(dest);
-	int i;
+	int index = 0;
+	int dest_len = 0;
 
-	for (i = 0; src[i] != '\0'; i++) {
-	dest[dest_len + i] = src[i];
-	}
-
-    	dest[dest_len + i] = '\0';
-
-    	return dest;
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
 }
