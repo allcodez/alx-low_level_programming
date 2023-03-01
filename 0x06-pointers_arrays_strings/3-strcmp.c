ece7fc2c7bc0774b4f6cfbcf91b3d107455fbec2
FahdAdebayo <fahdadebayo02@gmail.com>
2023-03-01 13:37:31 -0800

    C

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,26 +14,11 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-	s1++;
-	s2++;
+		s1++;
+		s2++;
 	}
 
-	if (*s1 == '\0' && *s2 == '\0')
-{
-	return 0;
-}
-	else if (*s1 == '\0')
-{
-	return -(*s2);
-	
-}	else if (*s2 == '\0')
-{
-	return *s1;
-}
-	else
-{
 	return (*s1 - *s2);
-}
 }
